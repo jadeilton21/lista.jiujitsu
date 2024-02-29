51c1640d9678e36781e2143429a4a71d09bd021f
Jadeilton <jadeiltondm@gmail.com>
2024-02-29 13:24:33 -0300

    feat: create class response

--- src/main/java/lista/academia/lista/jiujitsu/Service/JiuJitsuService.java
+++ src/main/java/lista/academia/lista/jiujitsu/Service/JiuJitsuService.java
@@ -2,6 +2,7 @@ package lista.academia.lista.jiujitsu.Service;
 
 import lista.academia.lista.jiujitsu.doMain.JiuJitsu;
 import lista.academia.lista.jiujitsu.feinCliente.JiuJitsuCliente;
+import lista.academia.lista.jiujitsu.response.JiuJitsuListResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
