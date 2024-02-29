76a4e4dc7589534667d8d8054756b526cb0611a3
Jadeilton <jadeiltondm@gmail.com>
2024-02-28 21:55:44 -0300

    feat:

--- src/main/java/lista/academia/lista/jiujitsu/controllers/JiuJitsuController.java
+++ src/main/java/lista/academia/lista/jiujitsu/controllers/JiuJitsuController.java
@@ -1,4 +1,14 @@
 package lista.academia.lista.jiujitsu.controllers;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/jiujitsu")
 public class JiuJitsuController {
+
+
+    private final JiuJitsuService  jiuJitsuService;
+
 }
