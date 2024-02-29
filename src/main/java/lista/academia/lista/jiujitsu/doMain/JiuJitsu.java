97d30655f9d4d258b6535b37f88cf3621e227585
Jadeilton <jadeiltondm@gmail.com>
2024-02-28 22:07:07 -0300

    feat: create class JiuJitsu

--- src/main/java/lista/academia/lista/jiujitsu/doMain/JiuJitsu.java
+++ src/main/java/lista/academia/lista/jiujitsu/doMain/JiuJitsu.java
@@ -1,6 +1,19 @@
 package lista.academia.lista.jiujitsu.doMain;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class JiuJitsu {
+
+
+    public String professor;
+    public String alunos;
+    public String local;
+    public String idadeMinima;
+    public String sexo;
 
 
 
