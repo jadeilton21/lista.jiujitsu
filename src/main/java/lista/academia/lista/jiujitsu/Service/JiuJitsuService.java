10e31a678ab759bcd5185ace04fd99fa03d85c55
Jadeilton <jadeiltondm@gmail.com>
2024-02-29 12:19:06 -0300

    feat: create method JiuJitsuService

--- src/main/java/lista/academia/lista/jiujitsu/Service/JiuJitsuService.java
+++ src/main/java/lista/academia/lista/jiujitsu/Service/JiuJitsuService.java
@@ -1,7 +1,17 @@
 package lista.academia.lista.jiujitsu.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JiuJitsuService {
+
+    @Autowired
+    privar final JiuJitsuCliente jiuJitsuCliente;
+
+
+
+    public JiuJitsuService(JiuJitsuService jiuJitsuService){
+        jiuJitsuService = jiuJitsuService;
+    }
 }
