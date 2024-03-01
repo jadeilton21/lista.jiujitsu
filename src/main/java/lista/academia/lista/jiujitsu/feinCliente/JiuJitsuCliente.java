e76d60d41b5446984edc810f806b04341b798e05
Jadeilton <jadeiltondm@gmail.com>
2024-03-01 18:10:21 -0300

    feat: create method JiuJitsu

--- src/main/java/lista/academia/lista/jiujitsu/feinCliente/JiuJitsuCliente.java
+++ src/main/java/lista/academia/lista/jiujitsu/feinCliente/JiuJitsuCliente.java
@@ -2,10 +2,11 @@ package lista.academia.lista.jiujitsu.feinCliente;
 
 import lista.academia.lista.jiujitsu.response.JiuJitsuListResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "jiujitsu-api",url = "")
 public interface JiuJitsuCliente() {
 
-
+    @GetMapping
     JiuJitsuListResponse getJiuJitsu();
 }
