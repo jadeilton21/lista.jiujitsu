5854323a68ea03367a8392b56458757a3928cb27
Jadeilton <jadeiltondm@gmail.com>
2024-03-01 18:11:45 -0300

    feat: create method in JiuJitsuListResponse

--- src/main/java/lista/academia/lista/jiujitsu/response/JiuJitsuListResponse.java
+++ src/main/java/lista/academia/lista/jiujitsu/response/JiuJitsuListResponse.java
@@ -1,4 +1,18 @@
 package lista.academia.lista.jiujitsu.response;
 
+
+import lista.academia.lista.jiujitsu.doMain.JiuJitsu;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class JiuJitsuListResponse {
+
+
+    private List<JiuJitsu> jiuJitsusList;
 }
