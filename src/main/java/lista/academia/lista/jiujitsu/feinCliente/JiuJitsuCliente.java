afc3f443a83135a6ac7bff4e532feab94f56d793
Jadeilton <jadeiltondm@gmail.com>
2024-03-06 22:03:39 -0300

    feat:

--- src/main/java/lista/academia/lista/jiujitsu/feinCliente/JiuJitsuCliente.java
+++ src/main/java/lista/academia/lista/jiujitsu/feinCliente/JiuJitsuCliente.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "jiujitsu-api",url = "https://academia-exemplo-api.s3.us-east-2.amazonaws.com/academias.Json")
 public interface JiuJitsuCliente {
-
     @GetMapping
     JiuJitsuListResponse getJiuJitsu();
 
